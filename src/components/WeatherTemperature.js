2178a0a0585afa73b5bb0d746ff307c259376ca4
Antonio Garcia <moy_357@hotmail.com>
2018-11-06 13:08:12 -0600

    Adding PropTypes dependency and use propTypes validation on WeatherTemperature Component, class 24.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+import PropTypes from 'prop-types';
 
 import {
   CLOUD,
@@ -35,6 +36,11 @@ const WeatherTemperature = ({temperature, weatherState}) => (
     }
     <span>{`${temperature} C`}</span>
   </div>
-)
+);
+
+WeatherTemperature.propTypes = {
+  temperature: PropTypes.number.isRequired,
+  weatherState: PropTypes.string.isRequired,
+};
 
 export default WeatherTemperature;

e1564577192b47cbf87eb9d653c12e4c031ca1dd
Antonio Garcia <moy_357@hotmail.com>
2018-12-13 17:49:18 -0600

    Section 4 class 34 (Dandole estilo).
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
-import './style.css';
+import './styles.css';
 
 const WeatherLocation = () => (
   <div className="weatherLocationCont">

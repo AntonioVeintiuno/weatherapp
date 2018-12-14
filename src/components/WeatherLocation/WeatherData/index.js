1da6b4ff53645b2834c69734358adf51d5068709
Antonio Garcia <moy_357@hotmail.com>
2018-12-13 18:12:17 -0600

    Section 5 class 36 double destructuring.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -4,13 +4,16 @@ import WeatherTemperature from './WeatherTemperature';
 import { SUN, } from './../../../constants/weathers';
 import './styles.css';
 
-const WeatherData = () => (
-  <div className="weatherDataCont">
-    <WeatherTemperature
-      temperature={20}
-      weatherState={SUN} />
-    <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
-  </div>
-);
+const WeatherData = ({ data }) => {
+  const { temperature, weatherState, humidity, wind } = data;
+  return (
+    <div className="weatherDataCont">
+      <WeatherTemperature
+        temperature={temperature}
+        weatherState={weatherState} />
+      <WeatherExtraInfo humidity={humidity} wind={wind} />
+    </div>
+  )
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import { SUN, } from './../../constants/weathers';
+
+const data = {
+  temperature: 7,
+  weatherState: SUN,
+  humidity: 10,
+  wind: '10 m/s',
+}
 
 const WeatherLocation = () => (
   <div className="weatherLocationCont">
     <Location city={"Monterrey"}></Location>
-    <WeatherData></WeatherData>
+    <WeatherData data={data}></WeatherData>
   </div>
 );
 

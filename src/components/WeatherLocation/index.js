aa63522a4f144d772e54ca09a46040eda292ee61
Antonio Garcia <moy_357@hotmail.com>
2019-05-12 15:31:32 -0600

    Adding param on url to convert data on celsius.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -8,7 +8,7 @@ const location = "Mexico,mx";
 const api_key = "9093edda048fb38f39b3a91bd9d9e8a4";
 const url_base_weather = "http://api.openweathermap.org/data/2.5/weather"
 
-const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}&units=metric`;
 
 const data = {
   temperature: 7,
